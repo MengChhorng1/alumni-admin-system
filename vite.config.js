f3d32f1035b3fabb4a8782dd6949bbf886509c8b
MengChhorng1 <chhorng349@gmail.com>
2026-05-23 02:04:00 +0700

    Update base path in Vite configuration to match the new project structure

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/alumni-admin-dashboard/',
+  base: '/alumni-admin-system/',
   plugins: [react(), tailwindcss()],
 });
